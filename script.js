1dbef76eab20020a2ebfc06faf4588460622c8ee
ar4wnnn <ar4wnn@gmail.com>
2023-04-11 23:24:17 +0200

    fix: Adjusted conditional statement order, added game function

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const possibleHands = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice() {
+function getComputerChoice() 
+{
     let randomIndex = Math.floor(Math.random() * possibleHands.length);
     let computerSelection = possibleHands[randomIndex];
     return computerSelection;
 }
 
-function getPlayerChoice() {
-    let playerSelection = prompt("What is your choice?", "rock/paper/scissors");
+function getPlayerChoice() 
+{
+    let playerSelection = prompt('What is your choice?', 'rock/paper/scissors');
     return playerSelection;
 }
 
 function playRound(playerChoice, computerChoice) 
 {
-    console.log('Computer chooses ' + computerChoice)
+    console.log('Computer chooses ' + computerChoice + '.')
     if ((playerChoice === 'scissors' && computerChoice === 'paper') ||
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
         (playerChoice === 'paper' && computerChoice === 'rock')) 
@@ -26,12 +28,23 @@ function playRound(playerChoice, computerChoice)
     {
         return 'You lost this round.'; 
     }
-    else if (!(playerChoice != possibleHands))
-    {
-        return 'You need to choose rock, paper or scissors only!';
-    }
-    else 
+    else if (playerChoice === computerChoice)
     {
         return 'It\'s a tie.';
     }
+    else if (playerChoice != possibleHands)
+    {
+        return 'You need to choose rock, paper or scissors only!';
+    }
+
 }
+
+function game() 
+{
+    for (let i = 0; i < 5; i++) 
+    {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+}
+
+game()
